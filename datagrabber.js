deace517a55b38af2c6d49ebfca68d8b938c340f
mr.Shu <mr@shu.io>
2015-10-03 19:11:19 +0200

    datagrabber: Only grab defined topics
    * Updated `datagrabber.js` to only grab the topics defined in
      tosdr.org/topics/
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- datagrabber.js
+++ datagrabber.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var dir = require('node-dir');
 var jf = require('jsonfile');
 var fs = require("fs");
@@ -6,18 +8,14 @@ String.prototype.endsWith = function(suffix) {
   return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
 
-dir.files('./tosdr.org/points/', function (err, files) {
+dir.files('./tosdr.org/topics/', function (err, files) {
   if (err) throw err;
-  var out = [];
+  var topics = [];
   (function next(){
     var path = files.pop();
 
     if (!files.length) {
-      fs.writeFile('tosdr-dataset.json', JSON.stringify(out), function (err) {
-        if (err) throw err;
-
-        console.log('Dataset prepared.');
-      });
+      dump(topics);
       return;
     }
 
@@ -26,21 +24,64 @@ dir.files('./tosdr.org/points/', function (err, files) {
       return;
     }
 
-    jf.readFile(path, function (err, obj) {
-      if (err) throw err;
+    path = path.replace(/.*\/([^\.]+)\.json/, "$1");
+    topics.push(path);
 
-      if (obj.topics !== undefined) {
-        var topic = obj.topics.pop();
-        var title = obj.title ? obj.title : '';
-        var description = obj.tosdr.tldr ? obj.tosdr.tldr : '';
-        out.push({
-          'topic': topic,
-          'title': title,
-          'description': description
-        });
-      }
-
-      next();
-    });
+    next();
   })();
 });
+
+function dump(topics) {
+  dir.files('./tosdr.org/points/', function (err, files) {
+    if (err) throw err;
+    var out = [];
+    (function next(){
+      var path = files.pop();
+
+      if (!files.length) {
+        fs.writeFile('tosdr-dataset.json', JSON.stringify(out), function (err) {
+          if (err) throw err;
+
+          console.log('Dataset prepared.');
+        });
+        return;
+      }
+
+      if (!path.endsWith('.json')) {
+        next();
+        return;
+      }
+
+      jf.readFile(path, function (err, obj) {
+        if (err) throw err;
+
+        if (obj.topics !== undefined) {
+          var topic = obj.topics.pop();
+          var title = obj.title ? obj.title : '';
+          var description = obj.tosdr.tldr ? obj.tosdr.tldr : '';
+
+          topic = topic.replace('user information', 'user-info');
+          topic = topic.replace('personal information', 'personal-data');
+          topic = topic.replace(' ', '-')
+          topic = topic.replace(/^scope.*license$/, 'copyright-scope');
+          topic = topic.replace(/^law.*gov.*/, 'law-gov');
+          topic = topic.replace(/^third.*/, 'third');
+          topic = topic.replace(/\%.*$/, '');
+
+          if (topics.indexOf(topic) == -1) {
+            next();
+            return;
+          }
+
+          out.push({
+            'topic': topic,
+            'title': title,
+            'description': description
+          });
+        }
+
+        next();
+      });
+    })();
+  });
+}
